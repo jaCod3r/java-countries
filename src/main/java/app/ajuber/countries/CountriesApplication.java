0fd3bea0b870b7e7356dd5a76b4ed742af49df0c
Juber Ahmed <a.juber@hotmail.com>
2019-02-12 08:40:48 -0500

    Population Controller almost done

--- src/main/java/app/ajuber/countries/CountriesApplication.java
+++ src/main/java/app/ajuber/countries/CountriesApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CountriesApplication {
 
+  static CountriesList ourCountryList;
+
   public static void main(String[] args) {
+    ourCountryList =  new CountriesList();
     SpringApplication.run(CountriesApplication.class, args);
   }
-
 }
 
--- src/main/java/app/ajuber/countries/PopulationController.java
+++ src/main/java/app/ajuber/countries/PopulationController.java
@@ -3,6 +3,26 @@ package app.ajuber.countries;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
+
 @RestController
 @RequestMapping("/population")
-public class PopulationController {}
+public class PopulationController {
+
+  Comparator<Country> comparator = Comparator.comparing( Country::getPopulation );
+
+  @RequestMapping("/min")
+  public Country getMinPopulation() {
+    return CountriesApplication.ourCountryList.countryList.stream().min(comparator).get();
+  }
+
+  @RequestMapping("/max")
+  public Country getMaxPopulation() {
+    return CountriesApplication.ourCountryList.countryList.stream().max(comparator).get();
+  }
+
+//  @RequestMapping("/size")
+//  public Country getPopulation(@RequestParam(value = "people") int population) {
+//    return CountriesApplication.ourCountryList.countryList
+//  }
+}
